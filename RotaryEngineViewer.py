d44ce603b44c6e158ebca768875f0a2f9ef74ed8
Maciej <maciektrucha@gmail.com>
2025-05-28 19:36:13 +0200

    poprawiona animacja

--- RotaryEngineViewer.py
+++ RotaryEngineViewer.py
@@ -4,7 +4,6 @@ from pyvistaqt import BackgroundPlotter
 from qtpy.QtWidgets import QApplication
 from qtpy.QtCore import QTimer
 
-
 class RotaryEngineViewer:
     def __init__(self, stl_folder):
         self.stl_folder = stl_folder
@@ -14,8 +13,7 @@ class RotaryEngineViewer:
         self._animation_thread = None
         self.original_meshes = {}
         self._angle = 0
-        self._orbit_radius = 0.01
-        self._delay = 10  # ms
+        self._delay = 0  # ms
 
 
     def load_parts(self):
@@ -57,14 +55,17 @@ class RotaryEngineViewer:
                 font_size=10,
                 name=f"label_{name}",
             )
+        
+    def add_animation_speed_slider(self):
+        #TODO: slider ktory zmienia wartosc degrees_per_step
+        # ewentualnie może dodawać self.delay gdy degrees_per step jest wystarczająco niskie
+        return
 
-
-    def toggle_animation(self, delay=0.01):
+    def toggle_animation(self):
         self.animating = not self.animating
         print(f"{'Starting' if self.animating else 'Stopping'} animation")
 
         if self.animating:
-            self._angle = 0
             self._animation_step()
 
 
@@ -73,7 +74,7 @@ class RotaryEngineViewer:
             return
 
         axis_vector = [0, 1, 0]
-        degrees_per_step = 5
+        degrees_per_step = 10
         crank_name = "crankshaft.stl"
         rotors = ["front_rotor.stl", "back_rotor.stl"]
 
@@ -85,15 +86,21 @@ class RotaryEngineViewer:
 
         for rotor_name in rotors:
             if rotor_name in self.components and rotor_name in self.original_meshes:
-                new_mesh = self.original_meshes[rotor_name].copy(deep=True)
-                transform = pv.transformations.axis_angle_rotation(axis_vector, self._angle, point=center)
-                new_mesh.transform(transform, inplace=True)
+                rotor_actor = self.components[rotor_name]
+                mesh = rotor_actor.GetMapper().GetInputAsDataSet()
 
-                # transform = pv.transformations.axis_angle_rotation(axis_vector, self._angle)
-                # new_mesh.transform(transform, inplace=True)
+                # Reset points from original
+                original = self.original_meshes[rotor_name]
+                mesh.points[:] = original.points.copy()
 
-                self.components[rotor_name].mapper.SetInputData(new_mesh)
-                self.components[rotor_name].mapper.Update()
+                # Apply transforms
+                t1 = pv.transformations.axis_angle_rotation(axis_vector, self._angle, point=center)
+                mesh.transform(t1, inplace=True)
+
+                rotor_center = mesh.center_of_mass()
+
+                t2 = pv.transformations.axis_angle_rotation(axis_vector, -self._angle * 2 / 3, point=rotor_center)
+                mesh.transform(t2, inplace=True)
 
         self._angle += degrees_per_step
         self.plotter.render()
@@ -104,12 +111,11 @@ class RotaryEngineViewer:
     def run(self):
         self.load_parts()
         self.add_checkboxes_with_labels()
-        self.plotter.add_key_event("space", lambda: self.toggle_animation(delay=0.01))
+        self.plotter.add_key_event("space", lambda: self.toggle_animation())
 
 
-if __name__ == "__main__":
-    viewer = RotaryEngineViewer("./models")
-    viewer.run()
+viewer = RotaryEngineViewer("./models")
+viewer.run()
 
-    app = QApplication.instance()
-    app.exec_()
+app = QApplication.instance()
+app.exec_()
